079dac9092a95aa5809805c87da3dbb6fa59dceb
Alan Mitchell <tabb99@gmail.com>
2014-07-14 10:06:07 -0800

    Added CPU temp to sys_info reader.

--- readers/sys_info.py
+++ readers/sys_info.py
@@ -9,13 +9,24 @@ class SysInfo(base_reader.Reader):
     
     def read(self):
 
+        # Uptime reading
         # Use the ID of this logger to create a unique ID.
         sensor_id = '%s_uptime' % (self._settings.LOGGER_ID)
         
         # parse seconds of uptime out of proc file.
         secs = float( open('/proc/uptime').read().split()[0] )
+        
+        ts = time.time()
 
-        return [(time.time(), sensor_id, int(secs), base_reader.COUNTER)]
+        readings = [(ts, sensor_id, int(secs), base_reader.COUNTER)]
+        
+        # CPU Temperature
+        sensor_id = '%s_cpu_temp' % (self._settings.LOGGER_ID)
+        cpu_temp = float(open('/sys/class/thermal/thermal_zone0/temp').read())/1000.0
+        if cpu_temp > -50.0 and cpu_temp < 150.0:
+            readings.append( (ts, sensor_id, cpu_temp, base_reader.VALUE) )
+            
+        return readings
 
 if __name__=='__main__':
     from pprint import pprint    

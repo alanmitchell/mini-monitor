952c9bcc0b8506d431cedcdf3049314314c59df8
Alan Mitchell <tabb99@gmail.com>
2014-09-04 12:45:23 -0800

    Added CRC8 checks to Device List & Temp commands

--- readers/linkUSB.py
+++ readers/linkUSB.py
@@ -6,6 +6,40 @@ import logging, time, re
 import serial
 import base_reader
 
+# ----------- Dallas 1-wire CRC Calculations -------------
+
+crc8_table = [
+0, 94, 188, 226, 97, 63, 221, 131, 194, 156, 126, 32, 163, 253, 31, 65,
+157, 195, 33, 127, 252, 162, 64, 30, 95, 1, 227, 189, 62, 96, 130, 220,
+35, 125, 159, 193, 66, 28, 254, 160, 225, 191, 93, 3, 128, 222, 60, 98,
+190, 224, 2, 92, 223, 129, 99, 61, 124, 34, 192, 158, 29, 67, 161, 255,
+70, 24, 250, 164, 39, 121, 155, 197, 132, 218, 56, 102, 229, 187, 89, 7,
+219, 133, 103, 57, 186, 228, 6, 88, 25, 71, 165, 251, 120, 38, 196, 154,
+101, 59, 217, 135, 4, 90, 184, 230, 167, 249, 27, 69, 198, 152, 122, 36,
+248, 166, 68, 26, 153, 199, 37, 123, 58, 100, 134, 216, 91, 5, 231, 185,
+140, 210, 48, 110, 237, 179, 81, 15, 78, 16, 242, 172, 47, 113, 147, 205,
+17, 79, 173, 243, 112, 46, 204, 146, 211, 141, 111, 49, 178, 236, 14, 80,
+175, 241, 19, 77, 206, 144, 114, 44, 109, 51, 209, 143, 12, 82, 176, 238,
+50, 108, 142, 208, 83, 13, 239, 177, 240, 174, 76, 18, 145, 207, 45, 115,
+202, 148, 118, 40, 171, 245, 23, 73, 8, 86, 180, 234, 105, 55, 213, 139,
+87, 9, 235, 181, 54, 104, 138, 212, 149, 203, 41, 119, 244, 170, 72, 22,
+233, 183, 85, 11, 136, 214, 52, 106, 43, 117, 151, 201, 74, 20, 246, 168,
+116, 42, 200, 150, 21, 75, 169, 247, 182, 232, 10, 84, 215, 137, 107, 53]
+
+def crc8_is_OK(hex_string):
+    """Returns True if the hex_string ending in a CRC byte passes the
+    Dallas 1-wire CRC8 check.
+    Code adapted from:  https://forum.sparkfun.com/viewtopic.php?p=51145.
+    """
+    # break the Hex string into a list of bytes
+    byte_list = [int(hex_string[i:i+2], 16) for i in range(0, len(hex_string), 2)]
+    val = 0
+    for x in byte_list:
+        val = crc8_table[val ^ x]
+    # answer should be 0 if the byte string is valid
+    return val==0
+
+# --------- End CRC Calculations ---------------
 
 class LinkUSBreader(base_reader.Reader):
     """Class that reads the sensors on a 1-wire bus with a LinkUSB master.
@@ -66,14 +100,18 @@ class LinkUSBreader(base_reader.Reader):
         while r:
             indicator, r_addr = r.groups()
             rec = {}
-            rec['family'] = r_addr[-2:]   # family code
+            
             # unreverse the address
             rec['addr'] = ''
             for i in range(0, 16, 2):
                 rec['addr'] += r_addr[14-i:14-i+2]
-            # make the id in the format used by the BMS system
-            rec['id'] = '%s.%s' % (rec['family'], rec['addr'][2:-2])
-            dev_list.append(rec)
+
+            # only add this to the list if the CRC passes
+            if crc8_is_OK(rec['addr']):
+                rec['family'] = r_addr[-2:]   # family code
+                # make the id in the format used by the BMS system
+                rec['id'] = '%s.%s' % (rec['family'], rec['addr'][2:-2])
+                dev_list.append(rec)
 
             # a minus sign indicates this is the last found device
             # on the bus.
@@ -85,7 +123,7 @@ class LinkUSBreader(base_reader.Reader):
         
         return dev_list            
 
-    regex_temp = re.compile('^[0-9A-F]{4}$')
+    regex_temp = re.compile('^[0-9A-F]{18}$')
     def readTemp(self, port, addr):
         """Returns the temperature in Fahrenheit read from the Dallas
         DS18B20 with the 16 hex digit ROM code of 'addr'.  'port' is an open
@@ -96,16 +134,18 @@ class LinkUSBreader(base_reader.Reader):
         port.write('\rrb55' + addr + 'BE')
         time.sleep(.1)   # appears to be critical! and does not work at 0.04 sleep
         port.flushInput()
-        port.write('FFFF')
-        lsb_msb = port.read(4)
+        port.write('FF'*9)   # read entire Scratchpad in order to enable CRC check
+        ret = port.read(18)
         port.write('\r')    # back to ASCII mode
-        
-        # Make sure return value from device is valid.  Don't allow 'FFFF'
-        # which is actually a valid return, but it also occurs when no 
-        # device responds to the read request.
-        if lsb_msb!='FFFF' and LinkUSBreader.regex_temp.search(lsb_msb):
-            sign = lsb_msb[2]   # the sign HEX digit
-            val = int(lsb_msb[3] + lsb_msb[:2], 16)
+
+        # Make sure return value from device is valid.
+        if LinkUSBreader.regex_temp.search(ret):
+            # Ensure CRC is OK
+            if not crc8_is_OK(ret):
+                raise Exception('Bad CRC calculating temperature for %s. Return bytes were: %s' % (addr, ret))
+
+            sign = ret[2]   # the sign HEX digit
+            val = int(ret[3] + ret[:2], 16)
             if sign=='F':
                 # this is a negative temperature expressed in 
                 # 12-bit twos complement
@@ -114,7 +154,7 @@ class LinkUSBreader(base_reader.Reader):
             return 32.0 + 1.8 * val/16.0
         
         else:
-            raise Exception('Bad 1-wire Temperature Return Value: %s' % lsb_msb)
+            raise Exception('Bad 1-wire Temperature Return Value: %s' % ret)
 
 
     regex_io = re.compile('^[0-9A-F]{2}$')

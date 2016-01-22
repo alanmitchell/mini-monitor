880efe478e1afa0aeb2324c0b0875e424cf4d47f
Alan Mitchell <tabb99@gmail.com>
2016-01-21 19:53:42 -0900

    Updated Version Number.

--- pi_logger.py
+++ pi_logger.py
@@ -16,9 +16,10 @@ import settings
 # ************* KEEP THIS UPDATED AS CHANGES ARE MADE ******************
 # Set the Software Version number as a property on the Settings module
 #
+# Version 1.5:  Added posting of IP Addresses in initial debug output.
 # Version 1.4:  Added Sensaphone reader class.
 #
-settings.VERSION = 1.4
+settings.VERSION = 1.5
 #***********************************************************************
 
 # The full directory path to this script file

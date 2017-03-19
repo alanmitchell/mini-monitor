1ffacd34091e6d2143f49251e6b682895610a8bd
Alan Mitchell <tabb99@gmail.com>
2017-03-18 18:30:41 -0800

    Fixed MQTT connect call.

--- mqtt_to_bmon.py
+++ mqtt_to_bmon.py
@@ -112,7 +112,7 @@ client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
  
-client.connect()
+client.connect('localhost')
  
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.

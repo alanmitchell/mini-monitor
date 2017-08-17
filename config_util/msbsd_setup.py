4bff9bbdb7665a6f766ef7bb20fcfb4a3daf892a
Alan Mitchell <tabb99@gmail.com>
2017-08-17 12:55:00 -0800

    Added Logger ID question to setup.

--- config_util/msbsd_setup.py
+++ config_util/msbsd_setup.py
@@ -8,6 +8,21 @@ from distutils.util import strtobool
 import io
 import setup_utils
 
+logger_id = raw_input('Enter a short (< 15 character) name for this site (spaces will be removed): ')
+logger_id = logger_id.replace(' ', '')
+
+# read settings file so it can be modified
+file_contents = open('pi_logger/settings.py').read()
+file_contents = setup_utils.replace_setting(file_contents, 'LOGGER_ID', '"%s"' % logger_id)
+
+meter_ids = raw_input('Enter Gas Meter ID or multiple IDs separated by commas: ')
+file_contents = setup_utils.replace_setting(file_contents, 'METER_IDS', '[%s]' % meter_ids)
+
+# Write the revised settings file.
+with io.open('pi_logger/settings.py', 'w', newline='\n') as f:
+    f.write(unicode(file_contents))
+
+# Configure WiFi, if requested.
 error = True
 while error:
     try:
@@ -24,8 +39,3 @@ while error:
 with io.open('pi_logger/wpa_supplicant.conf', 'w', newline='\n') as f:
     f.write(unicode(file_contents))
 
-meter_ids = raw_input('Enter Gas Meter ID or multiple IDs separated by commas: ')
-file_contents = open('pi_logger/settings.py').read()
-file_contents = setup_utils.replace_setting(file_contents, 'METER_IDS', '[%s]' % meter_ids)
-with io.open('pi_logger/settings.py', 'w', newline='\n') as f:
-    f.write(unicode(file_contents))

85c4e697ddd5bb13fe96f375451dab1c554cfa8d
Alan Mitchell <tabb99@gmail.com>
2018-01-14 21:02:46 -0900

    Rearranged script.

--- rtl433_reader.py
+++ rtl433_reader.py
@@ -61,27 +61,6 @@ def shutdown(signum, frame):
 signal.signal(signal.SIGTERM, shutdown)
 signal.signal(signal.SIGINT, shutdown)
 
-# Main Script body here
-
-try:
-    # Start the RTL_433 receiver
-    rtl_receiver = RTLreceiver()
-    rtl_receiver.start()
-
-    # Start up the object that will post the final readings to the MQTT
-    # broker.
-    mqtt = mqtt_poster.MQTTposter()
-    mqtt.start()
-
-    # determine the time when the next summarized post will occur
-    next_log_time = time.time() + settings.LOG_INTERVAL
-
-    # get the base Logger ID from the settings file
-    logger_id = settings.LOGGER_ID
-
-except:
-    logging.exception('Error initializing the script.')
-
 # create a dictionary to hold all the readings collected during a
 # logging interval.  The keys of the dictionary will be the reading ID,
 # the values will be a list of readings, each reading being a tuple of the form:
@@ -108,6 +87,27 @@ def add_readings(ts, id, values):
     read_list.append( (ts, val_rh))
     final_read_data[final_id] = read_list
 
+# Main Script body here
+
+try:
+    # Start the RTL_433 receiver
+    rtl_receiver = RTLreceiver()
+    rtl_receiver.start()
+
+    # Start up the object that will post the final readings to the MQTT
+    # broker.
+    mqtt = mqtt_poster.MQTTposter()
+    mqtt.start()
+
+    # determine the time when the next summarized post will occur
+    next_log_time = time.time() + settings.LOG_INTERVAL
+
+    # get the base Logger ID from the settings file
+    logger_id = settings.LOGGER_ID
+
+except:
+    logging.exception('Error initializing the script.')
+    sys.exit(1)
 
 # These hold the raw readings coming from the RTL-SDR receiver.  The keys
 # are a two-tuple (date/time string, id).  The values are a list of two-tuples
@@ -120,6 +120,7 @@ readings = {}
 while True:
     try:
         time.sleep(1)
+        lin = ''
         try:
             if rtl_receiver.readings_available():
                 lin = rtl_receiver.get_reading()
